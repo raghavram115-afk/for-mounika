6966ace57fea0c2b3fe841c29ad696662d815e0b
raghavram115-afk <raghavram115@gmail.com>
2026-02-14 15:37:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,75 @@
-// ----------- PHOTO SLIDESHOW -----------
+window.onload = function () {
 
-const photos = [
-  "photo1.jpeg",
-  "photo2.jpeg",
-  "photo3.jpeg",
-  "photo4.jpeg",
-  "photo5.jpeg"
-];
+  // ----------- PHOTO SLIDESHOW -----------
 
-let index = 0;
-const photoElement = document.getElementById("photo");
+  const photos = [
+    "photo1.jpeg",
+    "photo2.jpeg",
+    "photo3.jpeg",
+    "photo4.jpeg",
+    "photo5.jpeg"
+  ];
 
-function showNextPhoto() {
-  photoElement.style.opacity = 0;
+  let index = 0;
+  const photoElement = document.getElementById("photo");
 
-  setTimeout(() => {
-    index = (index + 1) % photos.length;
-    photoElement.src = photos[index];
-    photoElement.style.opacity = 1;
-  }, 500);
-}
+  if (photoElement) {
+    function showNextPhoto() {
+      photoElement.style.opacity = 0;
 
-setInterval(showNextPhoto, 3000);
+      setTimeout(() => {
+        index = (index + 1) % photos.length;
+        photoElement.src = photos[index];
+        photoElement.style.opacity = 1;
+      }, 500);
+    }
 
-
-// ----------- TYPING MESSAGE -----------
-
-const message = "the day I first saw you changed something inside me. I am feeling that you are distancing lately. I did something but my intention was never to hurt you.";
-
-const typingElement = document.getElementById("typingText");
-
-let i = 0;
-
-function typeWriter() {
-  if (i < message.length) {
-    typingElement.innerHTML += message.charAt(i);
-    i++;
-    setTimeout(typeWriter, 50);
+    setInterval(showNextPhoto, 3000);
   }
-}
 
-typeWriter();
+  // ----------- TYPING MESSAGE -----------
 
+  const message = "the day I first saw you changed something inside me. I am feeling that you are distancing lately. I did something but my intention was never to hurt you.";
+
+  const typingElement = document.getElementById("typingText");
+
+  if (typingElement) {
+    let i = 0;
+
+    function typeWriter() {
+      if (i < message.length) {
+        typingElement.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 50);
+      }
+    }
+
+    typeWriter();
+  }
+
+  // ----------- MUSIC BUTTON -----------
+
+  const music = document.getElementById("bgMusic");
+  const playBtn = document.getElementById("playMusicBtn");
+
+  if (playBtn && music) {
+    playBtn.addEventListener("click", function () {
+      music.play();
+      music.volume = 0.3;
+      playBtn.style.display = "none";
+    });
+  }
+
+};
 
 // ----------- VALENTINE QUESTION -----------
 
 function showValentineQuestion() {
-  document.getElementById("mainContent").style.display = "none";
-  document.getElementById("valentineSection").style.display = "block";
-}
+  const main = document.getElementById("mainContent");
+  const valentine = document.getElementById("valentineSection");
 
-
-// ----------- MUSIC BUTTON -----------
-
-const music = document.getElementById("bgMusic");
-const playBtn = document.getElementById("playMusicBtn");
-
-if (playBtn) {
-  playBtn.addEventListener("click", function() {
-    music.play();
-    music.volume = 0.3;
-    playBtn.style.display = "none";
-  });
+  if (main && valentine) {
+    main.style.display = "none";
+    valentine.style.display = "block";
+  }
 }
