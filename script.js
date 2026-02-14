82978e8ede7a71969ed354390ace5956d12f9e81
raghavram115-afk <raghavram115@gmail.com>
2026-02-14 18:11:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 window.onload = function () {
 
+  // ----------- FALLING HEARTS -----------
+
+const heartsContainer = document.querySelector(".hearts");
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+  heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+
+  heartsContainer.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+setInterval(createHeart, 300);
+
+
   // ----------- PHOTO SLIDESHOW -----------
 
   const photos = [
